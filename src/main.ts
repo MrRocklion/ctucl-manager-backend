3ba87f4d184d07104e4790d95e6c910f555258cd
David Diaz <ctucl-desarrollo@outlook.com>
2025-02-25 11:38:24 -0500

    test2

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Deshabilitar CORS completamente
   app.enableCors({
-    origin: ['http://localhost:3000','ctucl-manager-frontend.vercel.app'], // 👈 Permite el frontend
+    origin: ['http://localhost:3000','https://ctucl-manager-frontend.vercel.app'], // 👈 Permite el frontend
     credentials: true, // 👈 Necesario para enviar cookies
     allowedHeaders: ['Content-Type', 'Authorization'], // Asegúrate de incluir todos los encabezados necesarios
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
