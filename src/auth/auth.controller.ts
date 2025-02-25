689ed6f2be297cfbaf102d9ba7b0a05f9559342c
David Diaz <ctucl-desarrollo@outlook.com>
2025-02-25 09:22:54 -0500

    t122

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
     if (token?.access) {
       res.cookie('token', token.access_token, {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: 'strict',
       });
       //ojala esta vex funcione
